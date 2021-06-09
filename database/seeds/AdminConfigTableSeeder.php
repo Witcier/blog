94cf94eccda2ca924c290835a569fa0e4e774919
Witcier <770201276@qq.com>
2021-06-09 18:00:21 +0800

    laravel 版本升级，修改配置的seeder文件

--- database/seeds/AdminConfigTableSeeder.php
+++ database/seeds/AdminConfigTableSeeder.php
@@ -24,7 +24,7 @@ class AdminConfigTableSeeder extends Seeder
                 'id' => 1,
                 'name' => 'user_avatar',
                 'updated_at' => '2020-05-31 15:10:40',
-                'value' => 'https://upload.jianshu.io/users/upload_avatars/16750626/131a68d1-c1b7-4942-b60e-6671ddcc7df9.jpg',
+                'value' => 'http://blog.witcier.com/media-store/img/witcier_avatar.jpg',
             ),
             1 => 
             array (
@@ -42,7 +42,7 @@ class AdminConfigTableSeeder extends Seeder
                 'id' => 3,
                 'name' => 'username',
                 'updated_at' => '2020-05-31 15:11:22',
-                'value' => 'techflowing',
+                'value' => 'Witcier',
             ),
             3 => 
             array (
@@ -51,7 +51,7 @@ class AdminConfigTableSeeder extends Seeder
                 'id' => 4,
                 'name' => 'user_github',
                 'updated_at' => '2020-06-18 17:20:44',
-                'value' => 'https://github.com/techflowing',
+                'value' => 'https://github.com/witcier',
             ),
             4 => 
             array (
@@ -60,7 +60,7 @@ class AdminConfigTableSeeder extends Seeder
                 'id' => 5,
                 'name' => 'user_email',
                 'updated_at' => '2020-06-18 17:21:17',
-                'value' => 'techflowing@gmail.com',
+                'value' => '770201276@qq.com',
             ),
             5 => 
             array (
@@ -69,7 +69,7 @@ class AdminConfigTableSeeder extends Seeder
                 'id' => 6,
                 'name' => 'user_wechat',
                 'updated_at' => '2020-06-18 17:27:17',
-                'value' => 'http://techflowing.cn/media-store/wechat.jpeg',
+                'value' => 'http://blog.witcier.com/media-store/img/QRCode.jpg',
             ),
             6 => 
             array (
@@ -87,33 +87,27 @@ class AdminConfigTableSeeder extends Seeder
                 'id' => 8,
                 'name' => 'about_site',
                 'updated_at' => '2020-10-20 09:12:19',
-                'value' => '##### 缘起
+                'value' => '##### 启程
 
-闲逛 GitHub 时发现一个网址导航网站项目 [WebStack-Laravel](https://github.com/hui-ho/WebStack-Laravel "WebStack-Laravel")，觉着这项目有意思，搞下试试，折腾尝试部署，成功；然后顺带看了眼 Laravel 官方文档，哎，这框架牛逼，得好好看看。与此同时，我正在找一款能支持无限分级、目录和文件同时存在的，可云端同步的笔记应用，无果，对比多家后都不是很满意。算了，自己写一个吧，正好把之前的 Hexo 博客也一波给干掉，搞起！
-
-理想很丰满，现实很骨感，PHP、大前端技能基本为零，项目写起来基本处于写一行查一行的状况，各种找示例参考，痛并快乐着！
+上班时闲逛 Laravel 社区发现了一个很牛的个人博客项目[techflowing-Blog](https://github.com/techflowing/Blog)，就把项目 clone 了，然后部署成功、运行，发现自己的个人博客网站瞬间弱爆了。然后便想着要不然把自己原来的博客网站给干掉吧，然后基于[techflowing-Blog](https://github.com/techflowing/Blog)重新开发一个适合自己的个人博客网站。
 
 
-##### 大事记
+##### 改造
 
-* 2020/10/11，开源
-* 2020/06/25，添加留言板模块
-* 2020/06/24，添加思维导图模块
-* 2020/06/19，完成关于模块
-* 2020/05/31，添加首页
-* 2020/05/30，完成博客模块
-* 2020/04/16，整体修改为黑色主题
-* 2020/04/13，完成 Wiki 模块
-* 2020/03/22，完成导航站模块
-* 2020/03/21，初始化项目
+* 升级 [Laravel](https://learnku.com/docs/laravel/8.x) 框架为 8.*
+* 将 [Laravel-admin](https://laravel-admin.org/) 改成 [Dcat Admin](https://learnku.com/docs/dcat-admin/2.x)
+* 本地线上将采用统一环境使用 [Laravel Sail](https://learnku.com/docs/laravel/8.x/sail/9789)
+* 打造一个适合自己的博客主题（这个放在最后是因为自己前端知识比较薄弱）
+
 
 ##### 已知问题
 
 1. 浏览器适配（当前仅适配 Chrome）
+2. bower install 被执行时会出现个别扩展无法正常安装，建议手动一个个安装
 
 ##### 致谢
 
-* [Laravel 7](https://learnku.com/docs/laravel/7.x "Laravel 7")
+* [Laravel](https://learnku.com/laravel)
 * [WebStack-Laravel](https://github.com/hui-ho/WebStack-Laravel "WebStack-Laravel")
 * [Laravel-admin](https://laravel-admin.org/ "Laravel-admin")
 * [Editor.md](http://editor.md.ipandao.com/ "Editor.md")
@@ -121,7 +115,9 @@ class AdminConfigTableSeeder extends Seeder
 * [zTree](http://www.treejs.cn/v3/main.php#_zTreeInfo "zTree")
 * [SmartWiki](https://github.com/lifei6671/SmartWiki "SmartWiki")
 * [Valine](https://valine.js.org/)
-* [KityMinder](https://github.com/fex-team/kityminder)',
+* [KityMinder](https://github.com/fex-team/kityminder)
+* [Laravel Sail](https://learnku.com/docs/laravel/8.x/sail/9789)
+* [Dcat Admin](https://learnku.com/docs/dcat-admin/2.x)',
             ),
             8 => 
             array (
